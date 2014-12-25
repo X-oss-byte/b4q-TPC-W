ff246b5289b017ae2f12a73e8e3b015506ef6a30
¨coderfengyun¨ <chentienan@yeah.net>
2014-12-25 11:28:51 +0800

    remove the old project

--- Bench4Q-v1.3.1-source-code/src/org/bench4Q/console/ui/controlTree/.svn/text-base/Type.java.svn-base
+++
@@ -1,5 +0,0 @@
-package org.bench4Q.console.ui.controlTree;
-	
-public enum Type {all, VIP, normal};
-
-
--- Bench4Q-v1.3.1-source-code/src/org/bench4Q/console/ui/section/InteractionType.java
+++
@@ -1,3 +0,0 @@
-package org.bench4Q.console.ui.section;
-
-public enum InteractionType {all, Browse, Order}
